730f97bdfe20a08e38fc358c4e22a71ab0d93259
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2023-11-23 00:16:11 -0300

    commit incial da api

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const PORT = 3001;
+
+//config
+    //mongoose
+    mongoose.connect('mongodb://127.0.0.1:27017/bancofilmes').then(() => {
+        console.log('conectado ao banco de dados!!!');
+    }).catch((erro) => {
+        console.log('Erro ao se conectar com banco de dados, erro: '+erro);
+    })
+
+    //cors
+    app.use(cors());
+
+    //body-parser
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+//rotas
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'ola mundo'});
+})
+
+
+//server
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
